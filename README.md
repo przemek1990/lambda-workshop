49c7988bbe975ba5f3eba5a21bbb50e2e3f24af3
przemek <1302przemek@gmail.com>
2015-10-22 16:40:10 +0200

    - update readme - add homework skeleton class

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# lambda-workshop
+##Lambda workshop - example code
+
+* recsys.workshop.lambda.HelloWorld - hello world example
+* recsys.workshop.lambda.S3EventProcessor - homework template 
+
+##  Technology:
+
+* Java8
+* Gradle v2.4
+
+
+## BUILDING UBER JAR
+
+```
+gradle build
+```
+
+Build result in *build/libs*
---
+++ src/main/java/recsys/workshop/lambda/S3EventProcessor.java
@@ -0,0 +1,31 @@
+package recsys.workshop.lambda;
+
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.s3.event.S3EventNotification.S3EventNotificationRecord;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+public class S3EventProcessor implements RequestHandler<S3Event, String> {
+    @Override
+    public String handleRequest(S3Event s3event, Context context) {
+        S3EventNotificationRecord record = s3event.getRecords().get(0);
+
+        String srcBucket = record.getS3().getBucket().getName();
+        // Object key may have spaces or unicode non-ASCII characters.
+        String srcPath = record.getS3().getObject().getKey().replace('+', ' ');
+        try {
+            srcPath = URLDecoder.decode(srcPath, "UTF-8");
+
+            //PUT YOUR CODE HERE
+            //USEFUL LINK -> http://docs.aws.amazon.com/lambda/latest/dg/java-wt-s3-log-event-data.html
+
+            return null;
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
