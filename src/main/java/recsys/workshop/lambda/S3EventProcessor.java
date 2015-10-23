02f0140a6513f7c5c18c108cbc3bd04a5679d7dc
przemek <1302przemek@gmail.com>
2015-10-23 10:37:23 +0200

    - add loger to s3 event processor

--- src/main/java/recsys/workshop/lambda/S3EventProcessor.java
+++ src/main/java/recsys/workshop/lambda/S3EventProcessor.java
@@ -2,6 +2,7 @@ package recsys.workshop.lambda;
 
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
 import com.amazonaws.services.s3.event.S3EventNotification.S3EventNotificationRecord;
@@ -14,11 +15,16 @@ public class S3EventProcessor implements RequestHandler<S3Event, String> {
     public String handleRequest(S3Event s3event, Context context) {
         S3EventNotificationRecord record = s3event.getRecords().get(0);
 
+        LambdaLogger logger = context.getLogger();
+
         String srcBucket = record.getS3().getBucket().getName();
         // Object key may have spaces or unicode non-ASCII characters.
         String srcPath = record.getS3().getObject().getKey().replace('+', ' ');
         try {
             srcPath = URLDecoder.decode(srcPath, "UTF-8");
+
+            logger.log("Bucket: " + srcBucket);
+            logger.log("Path: " + srcPath);
 
             //PUT YOUR CODE HERE
             //USEFUL LINK -> http://docs.aws.amazon.com/lambda/latest/dg/java-wt-s3-log-event-data.html
